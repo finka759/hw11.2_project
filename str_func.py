0f29b6c4438c966f8facc3a2c53fe8117baf8856
finka759 <finka759@gmail.com>
2024-01-19 21:07:43 +0300

    11.2_3.4 Отправьте проделанную работу коллегам на код-ревью (т. е. pull-request, который должен показать, что есть конфликт: This branch has conflicts that must be resolved).

--- str_func.py
+++ str_func.py
@@ -1,2 +1,10 @@
 def remake_to_upper(str_):
     return str_.upper()
+
+def remake_to_title(str_):
+    '''
+    :param str_:
+    :return:
+    Добавьте к функции docstring.
+    '''
+    return str_.title()
